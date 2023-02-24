7c43810f0fcb6e498be60121096cd633d9d6c78e
JessicaSaito <jsayu.saito@gmail.com>
2023-02-24 11:08:19 -0300

    Atualizacao Controller e DataBase

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -1,19 +1,25 @@
 const mysql = require('mysql')
-const courseModel = require('../models/courseModel')
+const courseModel = {}
 
-const courseController = {}
-
-courseController.listAllCourses = (req, res) => {
-  courseModel.listAllCourses((error, result) => {
-    if (error)
-      res.status(500).json({ message: "Erro no Banco de Dados" })
-    if (result)
-      res.json(result)
+courseModel.listAllCourses = (callback) => {
+  const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "apinode"
+  })
+  const sql = "SELECT * FROM cursos;"
+  con.query(sql, (err, result) => {
+    if (err) {
+      callback(err, null)
+    } else {
+      callback(null, result)
+    }
   })
 }
 
-courseController.createCourse = (req, res) => {
-  courseModel.createCourse(req, res)
+courseModel.createCourse = (req, res) => {
+  res.json({ message: "Entrou na rota /course com POST!" })
 }
 
-module.exports = courseController
+module.exports = courseModel
