d199ad6ff8a66be492f044517517b800b452b261
JESSICA <cg3021751@LAB204M14.IFSP>
2023-03-24 17:10:46 -0300

    Tratamento de erro json invalido e criacao de Middleware errorHandler e logger

--- src/server.js
+++ src/server.js
@@ -3,20 +3,25 @@ import express from 'express'
 import courseRoutes from './routes/courseRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import { SERVER }  from './config.js'
+import errorHandler from './middlewares/errorHandler.js'
+import logger from './middlewares/logger.js'
 
 const app = express()
 const port = SERVER.PORT
 
+app.use(logger)
 app.use(express.json())
 
-//TODO: Tratar erro de json inválido
+//app.use(express.urlencoded({ extended: false })) // get form data urlenconded
 
 app.use('/course', courseRoutes)
 app.use('/user', userRoutes)
 
 app.all('*', (req, res) => {
-  res.status(404).send('404 Rota não encontrada!')
+  res.status(404).json({ message: '404 Rota não encontrada...' })
 })
+
+app.use(errorHandler)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
